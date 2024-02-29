0c6bf350154e6876e24fc8a155947c80e09fdd9e
Luis Perez <luisperez07182@email.com>
2024-02-28 20:30:37 -0600

    added seam and empty normal_tick method

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -7,7 +7,17 @@ class GildedRose
     @quality = quality
   end
 
+
+
   def tick
+    if name == 'Normal Item'
+      return normal_tick
+    end
+
+    def normal_tick
+    end
+
+
     if @name != "Aged Brie" and @name != "Backstage passes to a TAFKAL80ETC concert"
       if @quality > 0
         if @name != "Sulfuras, Hand of Ragnaros"
